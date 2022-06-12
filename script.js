4522c6a73ae6a8bfc3d17d8454f02ee89be603d9
Saurav Burjwal <sauravburjwal@gmail.com>
2022-06-12 17:26:09 +0530

    main

---
+++ script.js
@@ -0,0 +1,43 @@
+'use strict';
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    displayMessage('⛔ No number');
+  } else if (guess === secretNumber) {
+    document.querySelector('.number').textContent = secretNumber;
+    displayMessage('🎉 Correct Number');
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📉Too High' : '📈 Too Low');
+      score--;
+      document.querySelector('.score').textContent = score;
+
+      document.querySelector('body').style.backgroundColor = '#222';
+      document.querySelector('.number').textContent = '?';
+    } else {
+      displayMessage('💥 You lost the game!');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  displayMessage('Start guessing...');
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+});
